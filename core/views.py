0961b524397bc1568ada64657fb2076c734987c1
Awaismaz <awais_mazahir@hotmail.com>
2025-07-05 18:10:02 +0500

    update

--- core/views.py
+++ core/views.py
@@ -8,6 +8,7 @@ from django.conf import settings
 @xframe_options_exempt
 def serve_media(request, path):
     full_path = os.path.join(settings.MEDIA_ROOT, path)
+    print("Serving media via Django view:", path)
     if not os.path.exists(full_path):
         raise Http404("File not found.")
     return FileResponse(open(full_path, 'rb'))
